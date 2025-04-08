908fe13247aa7f669b70cb3f079f0292122c561f
Stphn <s.woirin.dev@gmail.com>
2025-04-08 18:28:41 +0200

    Feat: removed passwordhash from schema

--- backend/src/users/schemas.py
+++ backend/src/users/schemas.py
@@ -10,8 +10,7 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True, validate=[validate.Length(min=3, max=50), validate_username])
     email = fields.Email(required=True)
-    password_hash = fields.Str(dump_only=True)
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
-    
+
     password = fields.Str(load_only=True, required=True, validate=validate.Length(min=6))
